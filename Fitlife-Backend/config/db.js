ea49ee19708ce6cbe002f812e430bd76abea3fdb
kjyothimurali <kjyothimurali1105@gmail.com>
2026-07-18 15:54:28 +0530

    Updated the database details

--- Fitlife-Backend/config/db.js
+++ Fitlife-Backend/config/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'root',  // Change this if you have a different MySQL username
-  password: 'chandu@1602',  // Change this to your MySQL password
+  user: 'root',  
+  password: 'chandu@1602', 
   database: 'fitlife'
 });
 
